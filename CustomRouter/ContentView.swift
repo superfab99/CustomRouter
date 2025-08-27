748f761853ca5f5865419847eb5ce4c979f8442f
suyog sawant <itssuyog11@gmail.com>
2025-08-27 17:53:07 +0530

    initial commit

---
+++ CustomRouter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomRouter
+//
+//  Created by Suyog Sawant on 27/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! Let Learn Navigation")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomRouter/CustomRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRouterApp.swift
+//  CustomRouter
+//
+//  Created by Suyog Sawant on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
