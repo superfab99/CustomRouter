c11eb3142876f05964b42775ba242053ba911b25
suyog sawant <itssuyog11@gmail.com>
2025-08-27 18:21:43 +0530

    Added router core class and protocol

--- CustomRouter/ContentView.swift
+++ CustomRouter/ContentView.swift
@@ -7,17 +7,79 @@
 
 import SwiftUI
 
+struct AnyDestination: Hashable {
+    
+    let id = UUID().uuidString
+    var destination: AnyView
+    
+    init<T: View>(destination: T) {
+        self.destination = AnyView(destination)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: AnyDestination, rhs: AnyDestination) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+}
+
+protocol Router {
+    func presentScreen<T: View>(destination: T)
+    func presentScreen<T: View>(destination: () -> T)
+}
+
+struct RouterView<Content: View>: View, Router {
+    @State private var path: [AnyDestination] = []
+    @ViewBuilder var content: (Router) -> Content
+    
+    var body: some View {
+        NavigationStack(path: $path){
+            content(self)
+                .navigationDestination(for: AnyDestination.self) { value in
+                    value.destination
+                }
+        }
+    }
+    
+    func presentScreen<T: View>(destination: T) {
+        path.append(AnyDestination(destination: destination))
+    }
+    
+    func presentScreen<T: View>(destination: () -> T) {
+        path.append(AnyDestination(destination: destination()))
+    }
+}
+
 struct ContentView: View {
+    var body: some View {
+        RouterView { router in
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, HomeView")
+                Button("Go to Profile") {
+                    router.presentScreen(destination: Text("hello world"))
+                }
+            }
+        }
+    }
+}
+
+struct ProfileView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! Let Learn Navigation")
+            Text("Hello, ProfileView")
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
